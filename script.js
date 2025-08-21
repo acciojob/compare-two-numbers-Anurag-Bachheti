e3e915af0a0228777f65bb38a1eedf0ca98ffe32
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-21 20:13:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+const first = document.getElementById("one");
+const second = document.getElementById("two");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+	if(first.value === second.value){
+	alert{true}
+		}else{
+	alert(false)
+	}
+})
